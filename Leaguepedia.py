144fd85831faefa25197348c410150bb8a103ea8
Hao Li <50431940+LIMUXUALE0927@users.noreply.github.com>
2021-11-27 22:31:05 +0800

    Update Leaguepedia.py

--- Leaguepedia.py
+++ Leaguepedia.py
@@ -222,7 +222,7 @@ with col3:
 fig = go.Figure(go.Bar(
             x=team_ban.head(10)['Count'],
             y=team_ban.head(10)['Champion'],
-            name='队伍总体ban数据'
+            name='队伍总体ban数据',
             marker=dict(
         color='rgba(50, 171, 96, 0.6)',
         line=dict(
