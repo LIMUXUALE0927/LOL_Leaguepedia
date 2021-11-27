a9b30491f26aac4c94339f91f9ebae7e2a084f06
Hao Li <50431940+LIMUXUALE0927@users.noreply.github.com>
2021-11-27 18:17:23 +0800

    Update Leaguepedia.py

--- Leaguepedia.py
+++ Leaguepedia.py
@@ -120,7 +120,11 @@ data = SG.merge(BP, on='GameId')
 
 
 st.dataframe(data[['Tournament', 'Tab', 'DateTime UTC', 'Patch', 'Team1',
-             'Team2', 'WinTeam', 'Team1Bans', 'Team2Bans', 'Team1Picks', 'Team2Picks']])
+             'Team2', 'WinTeam', 'Team1Ban1', 'Team1Ban2', 'Team1Ban3', 'Team1Ban4', 'Team1Ban5',
+             'Team2Ban1', 'Team2Ban2', 'Team2Ban3', 'Team2Ban4', 'Team2Ban5',
+             'Team1Pick1', 'Team1Pick2', 'Team1Pick3', 'Team1Pick4', 'Team1Pick5',
+             'Team2Pick1', 'Team2Pick2', 'Team2Pick3', 'Team2Pick4', 'Team2Pick5',
+             'Team1PicksByRoleOrder', 'Team2PicksByRoleOrder']])
 
 
 @st.cache
