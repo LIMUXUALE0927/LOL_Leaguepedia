1de66296f073616281d9922f583f2bfc3050659b
Hao Li <50431940+LIMUXUALE0927@users.noreply.github.com>
2021-11-29 01:38:39 +0800

    Update Leaguepedia.py

--- Leaguepedia.py
+++ Leaguepedia.py
@@ -235,7 +235,7 @@ team_ban.columns = ['Champion', 'Count']
 col1, col2, col3 = st.columns(3)
 with col1:
     st.write('总体Ban数据：')
-    st.dataframe(team_ban)
+    st.dataframe(team_ban.style.background_gradient("Greens",subset="Count"))
 
 
 with col2:
